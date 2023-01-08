dd51456636ae5c7ed7594c6a0e4f9d5f6a13ec81
stev232 <stev232@yahoo.com>
2023-01-08 14:34:07 -0600

    Made it so you can search a city and display the weather for that city

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,44 +22,70 @@ const weatherEl = [[ document.getElementById('date'), document.getElementById('w
 var lat = 45.7452;
 var lon = -87.0646;
 var apiCall = '';
+var city;
+var state;
 
-apiCall = apiSite[0] + apiLat + lat + apiLon + lon + apiKey;
+function current() {
+    apiCall = apiSite[0] + apiLat + lat + apiLon + lon + apiKey;
+    
+    fetch(apiCall)
+    .then(function (response) {
+      localStorage.setItem('Weather', response);
+      return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        document.getElementById('city').textContent = city + ", " + state;
+        console.log(dayjs.unix(data.dt).format('M/D/YYYY'));
+    
+        weatherEl[0][0].textContent = dayjs.unix(data.dt).format('M/D/YYYY');
+        weatherEl[0][1].setAttribute('src', 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png');
+        weatherEl[0][2].textContent = 'Temp: ' + data.main.temp + '\xB0F';
+        weatherEl[0][3].textContent = 'wind: ' + data.wind.speed + ' MPH';
+        weatherEl[0][4].textContent = 'Humidity: ' + data.main.humidity + '%';
+    });
+}
 
-fetch(apiCall)
-.then(function (response) {
-  localStorage.setItem('Weather', response);
-  return response.json();
-})
-.then(function (data) {
-    console.log(data);
-    document.getElementById('city').textContent = data.name;
-    console.log(dayjs.unix(data.dt).format('M/D/YYYY'));
+function fiveDay() {
+    apiCall = apiSite[1] + apiLat + lat + apiLon + lon + apiKey;
+    
+    fetch(apiCall)
+    .then(function (response) {
+        localStorage.setItem('Weather', response);
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        //Loop over the data to generate a table, each table row will have a link to the repo url
+        for (var i = 1; i < 6; i++) {
+            console.log(i*8-1);
+            console.log(data.list[i*8-1].dt_txt);
+            weatherEl[i][0].textContent = dayjs(data.list[i*8-1].dt_txt).format('M/D/YYYY');
+            weatherEl[i][1].setAttribute('src', 'http://openweathermap.org/img/w/' + data.list[i*8-1].weather[0].icon + '.png');
+            weatherEl[i][2].textContent = 'Temp: ' + data.list[i*8-1].main.temp + '\xB0F';
+            weatherEl[i][3].textContent = 'wind: ' + data.list[i*8-1].wind.speed + ' MPH';
+            weatherEl[i][4].textContent = 'Humidity: ' + data.list[i*8-1].main.humidity + '%';
+        }
+    });
+}
 
-    weatherEl[0][0].textContent = dayjs.unix(data.dt).format('M/D/YYYY');
-    weatherEl[0][1].setAttribute('src', 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png');
-    weatherEl[0][2].textContent = 'Temp: ' + data.main.temp + '\xB0F';
-    weatherEl[0][3].textContent = 'wind: ' + data.wind.speed + ' MPH';
-    weatherEl[0][4].textContent = 'Humidity: ' + data.main.humidity + '%';
-});
+searchBtn.addEventListener('click', function() {
+    city = document.getElementById('cityInput').value;
+    apiCall = apiSite[2] + 'q=' + city + apiKey;
+    
+    fetch(apiCall)
+    .then(function (response) {
+        localStorage.setItem('city', response);
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        //Loop over the data to generate a table, each table row will have a link to the repo url
+        state = data[0].state;
+        lat = data[0].lat;
+        lon = data[0].lon;
 
-apiCall = apiSite[1] + apiLat + lat + apiLon + lon + apiKey;
-
-fetch(apiCall)
-.then(function (response) {
-    localStorage.setItem('Weather', response);
-    return response.json();
-})
-.then(function (data) {
-    console.log(data);
-    document.getElementById('city').textContent = data.city.name;
-    //Loop over the data to generate a table, each table row will have a link to the repo url
-    for (var i = 1; i < 6; i++) {
-        console.log(i*8-1);
-        console.log(data.list[i*8-1].dt_txt);
-        weatherEl[i][0].textContent = dayjs(data.list[i*8-1].dt_txt).format('M/D/YYYY');
-        weatherEl[i][1].setAttribute('src', 'http://openweathermap.org/img/w/' + data.list[i*8-1].weather[0].icon + '.png');
-        weatherEl[i][2].textContent = 'Temp: ' + data.list[i*8-1].main.temp + '\xB0F';
-        weatherEl[i][3].textContent = 'wind: ' + data.list[i*8-1].wind.speed + ' MPH';
-        weatherEl[i][4].textContent = 'Humidity: ' + data.list[i*8-1].main.humidity + '%';
-    }
+        current();
+        fiveDay();
+    });
 });
