a8d1fa1f63eb54fbee6723bdaaaf60e2127d7f16
stev232 <stev232@yahoo.com>
2023-01-08 21:05:36 -0600

    saving location search history in local storage with the most recent city looked up placed at the start of the array

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,7 @@ var lon = -87.0646;
 var apiCall = '';
 var city;
 var state;
-var arrCity = [[]];
+var arrCity;
 
 function current() {
     apiCall = apiSite[0] + apiLat + lat + apiLon + lon + apiKey;
@@ -35,7 +35,12 @@ function current() {
       return response.json();
     })
     .then(function (data) {
-        document.getElementById('city').textContent = city + ", " + state;
+        console.log(data);
+        if(city!=null) {
+            document.getElementById('city').textContent = city + ", " + state;
+        } else {
+            document.getElementById('city').textContent = data.name;
+        }
     
         weatherEl[0][0].textContent = dayjs.unix(data.dt).format('M/D/YYYY');
         weatherEl[0][1].setAttribute('src', 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png');
@@ -56,13 +61,12 @@ function fiveDay() {
     })
     .then(function (data) {
         for (var i = 1; i < 6; i++) {
-            console.log(data);
-            weatherEl[i][0].textContent = dayjs(data.list[i*8-1].dt_txt).format('M/D/YYYY');
-            weatherEl[i][1].setAttribute('src', 'http://openweathermap.org/img/w/' + data.list[i*8-1].weather[0].icon + '.png');
-            weatherEl[i][1].setAttribute('alt', data.list[i*8-1].weather[0].description);
-            weatherEl[i][2].textContent = 'Temp: ' + data.list[i*8-1].main.temp + '\xB0F';
-            weatherEl[i][3].textContent = 'wind: ' + data.list[i*8-1].wind.speed + ' MPH';
-            weatherEl[i][4].textContent = 'Humidity: ' + data.list[i*8-1].main.humidity + '%';
+            weatherEl[i][0].textContent = dayjs(data.list[i*8-4].dt_txt).format('M/D/YYYY');
+            weatherEl[i][1].setAttribute('src', 'http://openweathermap.org/img/w/' + data.list[i*8-4].weather[0].icon + '.png');
+            weatherEl[i][1].setAttribute('alt', data.list[i*8-4].weather[0].description);
+            weatherEl[i][2].textContent = 'Temp: ' + data.list[i*8-4].main.temp + '\xB0F';
+            weatherEl[i][3].textContent = 'wind: ' + data.list[i*8-4].wind.speed + ' MPH';
+            weatherEl[i][4].textContent = 'Humidity: ' + data.list[i*8-4].main.humidity + '%';
         }
     });
 }
@@ -78,13 +82,16 @@ function saveData() {
 }
 
 searchBtn.addEventListener('click', function() {
+    city = '';
     city = document.getElementById('cityInput').value;
 
-    for(var i = 0; i>arrCity.length; i++) {
-        if(city == arrCity[i][0]) {
-            state = arrCity[i][1];
-            lat = arrCity[i][2];
-            lon = arrCity[i][3];
+    if(arrCity != null) {
+        for(var i = 0; i>arrCity.length; i++) {
+            if(city == arrCity[i][0]) {
+                state = arrCity[i][1];
+                lat = arrCity[i][2];
+                lon = arrCity[i][3];
+            }
         }
     }
     apiCall = apiSite[2] + 'q=' + city + apiKey;
@@ -98,24 +105,27 @@ searchBtn.addEventListener('click', function() {
         state = data[0].state;
         lat = data[0].lat;
         lon = data[0].lon;
-
-        if(arrCity.length == 0) {
-            arrCity += [[city, data[0].state, data[0].lat, data[0].lon]];
+        if(arrCity==null) {
+            arrCity = [city, data[0].state, data[0].lat, data[0].lon];
         } else {
-            for(var i = 0; i>arrCity.length; i++) {
-                if(arrCity[i][0] == city) {
-                    arrCity;
-                } else {
-                    arrCity += [[city, data[0].state, data[0].lat, data[0].lon]];
-                }
-            }
+            arrCity = [city, data[0].state, data[0].lat, data[0].lon] + ',' + arrCity;
         }
-
-        localStorage.setItem('city', arrCity);
-
+        
+        saveData();
         current();
         fiveDay();
     });
 });
 
 loadData();
+
+if(arrCity != null) {
+    arrCity = arrCity.split(',');
+    city = arrCity[0];
+    state = arrCity[1];
+    lat = arrCity[2];
+    lon = arrCity[3];
+}
+
+current();
+fiveDay();
